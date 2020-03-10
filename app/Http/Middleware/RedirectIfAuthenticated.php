a468202e4852ec7a3bec32374fa7de69f2d105ef
Stoa <542950749@qq.com>
2020-03-10 12:14:14 +0800

    后台跳转

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,10 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            if($guard === 'admin'){
+                return redirect(env('ADMIN_PRE'));
+            }
+            return redirect('/');
         }
 
         return $next($request);
